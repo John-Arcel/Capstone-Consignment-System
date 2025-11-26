7f2a38a5ad88d8cdb155b43296cd7b4bcaf44079
znox-x <pastor.khyllejosh@gmail.com>
2025-11-26 18:33:51 +0800

    main program GUI (WIP)

---
+++ Capstone/src/MainProgram.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class MainProgram {
+    private JPanel panelMain;
+    private JPanel SideBarMenu;
+    private JLabel greetTextField;
+    private JButton dashboardButton;
+}
