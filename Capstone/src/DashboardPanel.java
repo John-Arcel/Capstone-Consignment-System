8b296ba767fe96a1d034bbb902189cc23702d158
LAB12_USER ON WS11 <L12Y08W11@LabHCI.net>
2025-11-27 08:28:13 +0800

    dashboard ui

--- Capstone/src/DashboardPanel.java
+++ Capstone/src/DashboardPanel.java
@@ -2,4 +2,7 @@ import javax.swing.*;
 
 public class DashboardPanel {
     private JTable table1;
+    private JTextField textField1;
+    private JButton findItemButton;
+    private JTable table2;
 }
--- Capstone/src/MainProgram.java
+++ Capstone/src/MainProgram.java
@@ -1,9 +1,17 @@
 import javax.swing.*;
 
-public class MainProgram {
+public class MainProgram extends JFrame{
     private JPanel panelMain;
     private JPanel SideBarMenu;
     private JLabel greetTextField;
     private JButton dashboardButton;
     private JPanel MainContentPanel;
+    private JLabel adminID;
+    private JButton inventoryButton;
+    private JButton transactionsButton;
+    private JButton payoutsButton;
+    private JButton logoutButton;
+
+    public MainProgram(){
+    }
 }
