52b8519199a88e2d4457b881607b458230a2889f
LAB12_USER ON WS10 <L12Y08W10@LabHCI.net>
2025-11-27 10:06:42 +0800

    Jpanel na assets (dili pa human tho libog pa)

--- Capstone/src/PayoutsPanel.java
+++ Capstone/src/PayoutsPanel.java
@@ -1,4 +1,22 @@
 import javax.swing.*;
 
-public class PayoutsPanel extends JPanel {
+public class PayoutsPanel extends JPanel{
+
+    PayoutsPanel(){
+        this.setSize(800,600);
+        this.setLayout(null);
+
+        String[] columnNames = {"Payout ID" , "Consignor" , "Paid" , "Date"};
+        Object[][] data = {{"P0000001", "P0000002", "P0000003"},{"Sally's shop", "GAP" , "Bench"},{10, 20 ,30} ,{"11/02/25", "11/03/25", "11/04/25"}};
+
+        JTable table = new JTable(data, columnNames);
+        table.setBounds(50,50,700,200);
+        this.add(table);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new PayoutsPanel();
+    }
+
 }
